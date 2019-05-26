932586e74729d160d5bd284bde24b2d2b1f0b279
CouldBeFree <propokerstar90@gmail.com>
2019-05-26 14:10:54 +0300

    commit

--- config/passport.js
+++ config/passport.js
@@ -11,9 +11,7 @@ module.exports = function (passport) {
             callbackURL:'/auth/google/callback',
             proxy: true
         }, (accessToken, refreshToken, profile, done) => {
-            //console.log(accessToken);
-            //console.log(profile);
-            const image = profile.photos[0].value;
+            const image = profile.photos[0].value.substring(0, profile.photos[0].value.indexOf('?'));
 
             const newUser = {
                 googleID: profile.id,
